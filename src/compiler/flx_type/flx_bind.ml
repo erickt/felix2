8fd5d17cfe1b0253ec6efbcc928ac13074c700d4
Erick Tryzelaar <idadesub@users.sourceforge.net>
2010-09-04 12:01:43 -0700

    Initial unification implementation

--- src/compiler/flx_type/flx_bind.ml
+++ src/compiler/flx_type/flx_bind.ml
@@ -1,4 +1,5 @@
 open Batteries
+open Format
 open Flx_format
 open Flx_type
 
@@ -13,6 +14,69 @@ module Ast_stmt = Flx_ast.Stmt
 (** Error out *)
 let error ?(sr=Flx_srcref.dummy_sr) format =
   ksprintf (fun s -> raise (Type_error (sr, s))) format
+
+
+(** Unify two types together. *)
+let rec unify tve typ1 typ2 =
+  unify' tve (tvchase tve typ1) (tvchase tve typ2)
+
+(** Chase through a substitution 'shallowly': stop at the last equivalent type
+ * variable. *)
+and tvchase tve typ =
+  let open Type in
+
+  match node typ with
+  | Variable v ->
+      begin match Flx_tve.find tve v with
+      | Some typ -> tvchase tve typ
+      | None -> typ
+      end
+  | _ -> typ
+
+(** If either typ1 or typ2 are type variables, they must be unbound. *)
+and unify' tve typ1 typ2 =
+  let open Type in
+
+  match node typ1, node typ2 with
+  | Integer kind1, Integer kind2 when kind1 = kind2 -> tve
+  | String, String -> tve
+  | Arrow (lhs1,rhs1), Arrow (lhs2,rhs2) ->
+      unify (unify tve rhs1 rhs2) lhs1 lhs2
+  | Variable var1, _ -> unify_free_variable tve var1 typ2
+  | _, Variable var2 -> unify_free_variable tve var2 typ1
+  | _, _ -> error (sr typ2)  "%a and %a" print typ1 print typ2
+
+and unify_free_variable tve var1 typ2 =
+  let open Type in
+
+  match node typ2 with
+  | Variable var2 ->
+      if var1 = var2 then tve else
+
+      (* Record a new constraint. *)
+      Flx_tve.add tve var1 typ2
+
+  | _ ->
+      if occurs tve var1 typ2 then
+        error (sr typ2) "occurs check: %a in %a"
+          print (variable var1)
+          print (Flx_tve.substitute tve typ2)
+      else
+        Flx_tve.add tve var1 typ2
+
+and occurs tve var1 typ2 =
+  let open Type in
+
+  match node typ2 with
+  | Integer _
+  | String -> false
+  | Arrow (lhs, rhs) -> occurs tve var1 lhs || occurs tve var1 rhs
+  | Variable var2 ->
+      begin match Flx_tve.find tve var2 with
+      | None -> var1 = var2
+      | Some typ2 -> occurs tve var1 typ2
+      end
+  | _ -> error (sr typ2) "occurs does not support %a yet" print typ2
 
 
 let bind_int_kind = function
@@ -31,7 +95,10 @@ let bind_type env tve typ =
       env, tve, typ
 
   (* Bind integer types. *)
+  (*
   | Int int_kind -> env, tve, Type.integer ~sr (bind_int_kind int_kind)
+  *)
+  | Int _ -> env, tve, Type.integer ~sr Type.Int
   | Name "int" -> env, tve, Type.integer ~sr Type.Int
   | Name "uint" -> env, tve, Type.integer ~sr Type.Uint
 
@@ -69,12 +136,20 @@ let rec bind_expr env tve expr =
       let typ_lhs = Expr.typ lhs in
       let typ_rhs = Expr.typ rhs in
 
+      let int_typ = Type.integer Type.Int in
+
+      printf "unifying:@ %a to@ %a@." Type.print typ_lhs Type.print typ_rhs;
+
+      let tve = unify (unify tve typ_lhs int_typ) typ_rhs int_typ in
+
+      (*
       (* Make sure all the types are the same. *)
       if not (Type.equals typ_lhs typ_rhs) then
         error ~sr:(Expr.sr rhs)
           "This expression has type@ %a but but expected type@ %a instead"
           Type.print typ_lhs
           Type.print typ_rhs;
+          *)
 
       env, tve, typ_lhs, lhs, rhs
   in
@@ -100,11 +175,14 @@ let rec bind_expr env tve expr =
 
   | Sum [lhs; rhs] ->
       let env, tve, typ, lhs, rhs = bind_binary_expr env tve lhs rhs in
+
       env, tve, Expr.make ~sr typ (Expr.Sum (lhs, rhs))
 
+  (*
   | Product [lhs; rhs] ->
       let env, tve, typ, lhs, rhs = bind_binary_expr env tve lhs rhs in
       env, tve, Expr.make ~sr typ (Expr.Product (lhs, rhs))
+  *)
 
   | _ -> error ~sr "Cannot bind expression:@ %a" Ast_expr.print expr
 
@@ -233,3 +311,12 @@ and bind_stmts env tve stmts =
     stmts
   in
   env, tve, List.rev stmts
+
+(* Bind and unify the types of a statement. *)
+let bind_stmt env tve stmt =
+  let open Stmt in
+
+  let env, tve, stmt = bind_stmt env tve stmt in
+  let typ' = Flx_tve.substitute tve (Stmt.typ stmt) in
+
+  env, tve, make ~sr:(sr stmt) typ' (node stmt)
