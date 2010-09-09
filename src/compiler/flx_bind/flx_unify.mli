091c1a0d96f9c5fcc3ab62a95306ef7a0a965c22
Erick Tryzelaar <idadesub@users.sourceforge.net>
2010-09-09 08:43:41 -0700

    Actually put flx_unify.mli in the right place.

--- src/compiler/flx_bind/flx_unify.mli
+++ src/compiler/flx_bind/flx_unify.mli
@@ -3,4 +3,4 @@ open Flx_type
 exception Unification_failed
 
 (** Unify two types together. *)
-val unify: ~sr:Flx_srcref.t -> Flx_tve.t -> Type.t -> Type.t
+val unify: sr:Flx_srcref.t -> Flx_tve.t -> Type.t -> Type.t -> Flx_tve.t
